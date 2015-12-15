b4cca71010c2a5e8714f3f64db451cfffb4044c6
Luccas Correa <luccascorrea@estudio89.com.br>
2015-12-15 00:32:53 -0200

    Bumped version number. Generated distribution package.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='e89-push-messaging',
-    version='1.0.18',
+    version='1.0.19',
     packages=['e89_push_messaging', 'e89_push_messaging.migrations'],
     include_package_data=True,
     license='BSD License',  # example license
